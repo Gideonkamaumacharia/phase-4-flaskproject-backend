a92d4f67d7a6e7f195b6cf32152124d54ed680e3
Gideon Macharia <kamaugedion95@gmail.com>
2024-07-09 14:54:51 +0300

    createmodel string  repr

--- model.py
+++ model.py
@@ -11,6 +11,9 @@ class User(db.Model, SerializerMixin):
     password = db.Column(db.String, nullable=False)
     surveys = db.relationship('Survey', back_populates='user')
 
+    def __repr__(self):
+        return f'<User id={self.id}, username={self.username}, email={self.email}>'
+
 class Survey(db.Model, SerializerMixin):
     __tablename__ = 'surveys'
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +23,9 @@ class Survey(db.Model, SerializerMixin):
     user = db.relationship('User', back_populates='surveys')
     questions = db.relationship('Question', back_populates='survey')
 
+    def __repr__(self):
+        return f'<Survey id={self.id}, title={self.title}>'
+
 class Question(db.Model, SerializerMixin):
     __tablename__ = 'questions'
     id = db.Column(db.Integer, primary_key=True)
@@ -27,6 +33,9 @@ class Question(db.Model, SerializerMixin):
     type = db.Column(db.String)
     survey_id = db.Column(db.Integer, db.ForeignKey('surveys.id'))
     survey = db.relationship('Survey', back_populates='questions')
+
+    def __repr__(self):
+        return f'<Question id={self.id}, content={self.content}, type={self.type}>'
 
 class Participant(db.Model, SerializerMixin):
     __tablename__ = 'participants'
@@ -36,3 +45,6 @@ class Participant(db.Model, SerializerMixin):
     completed = db.Column(db.Boolean, default=False)
     user = db.relationship('User', backref='participations')
     survey = db.relationship('Survey', backref='participants')
+
+    def __repr__(self):
+        return f'<Participant id={self.id}, user_id={self.user_id}, survey_id={self.survey_id}, completed={self.completed}>'
