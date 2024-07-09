a5e65e940b354d9c57cacca0799bef4877d95e9a
Gideon Macharia <kamaugedion95@gmail.com>
2024-07-09 14:51:19 +0300

    debug models

--- model.py
+++ model.py
@@ -3,34 +3,36 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class User(db.Model,SerializerMixin):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    email= db.Column(db.String)
-    password = db.Column(db.String)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
     surveys = db.relationship('Survey', back_populates='user')
 
-class Survey(db.Model,SerializerMixin):
-    __tablename__='surveys'
-    id= db.Column(db.Integer,primary_key=True)
-    title= db.Column(db.String)
-    description = db.Column(db.String)
-    user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
-    user= db.relationship('User',back_populates='surveys')
-    questions = db.relationship('Qustion',back_populates='survey')
-
-class Question(db.Model,SerializerMixin):
-    __tablename__ ='questions'
+class Survey(db.Model, SerializerMixin):
+    __tablename__ = 'surveys'
     id = db.Column(db.Integer, primary_key=True)
-    content= db.Column(db.String)
-    type= db.Column(db.String)
-    survey_id=db.Column(db.Integer,db.ForeignKey('surveys.id'))
-    survey = db.relationship('Survey',back_populates='questions')
-
-class Participant(db.Model,SerializerMixin):
-    __tablename__ ='participants'
-    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    survey_id= db.Column(db.Integer, db.ForeignKey('surveys.id'))
-    completed= db.Column(db.Boolean,default=False)
+    user = db.relationship('User', back_populates='surveys')
+    questions = db.relationship('Question', back_populates='survey')
+
+class Question(db.Model, SerializerMixin):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String)
+    type = db.Column(db.String)
+    survey_id = db.Column(db.Integer, db.ForeignKey('surveys.id'))
+    survey = db.relationship('Survey', back_populates='questions')
+
+class Participant(db.Model, SerializerMixin):
+    __tablename__ = 'participants'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    survey_id = db.Column(db.Integer, db.ForeignKey('surveys.id'))
+    completed = db.Column(db.Boolean, default=False)
+    user = db.relationship('User', backref='participations')
+    survey = db.relationship('Survey', backref='participants')
